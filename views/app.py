675c6cba5f3829358a2c7ef73213aea1897ff12b
cqveman <trymemain.pb.hug@gmail.com>
2026-02-26 09:17:07 +0200

    Refactor register view and main app

--- views/app.py
+++ views/app.py
@@ -1,142 +1,39 @@
 import ttkbootstrap as tb
+import tkinter as tk
 
 from controllers.user_controller import UserController
+from views.register_view import RegisterView
 
 
 class App(tb.Window):
+    WIDTH = 800
+    HEIGHT = 840
+
     def __init__(self, user_service):
         super().__init__()
-        self.__user_controller = UserController(self, user_service)
+        self.user_controller = UserController(self, user_service)
 
         self.iconbitmap('./app.ico')
         self.title('Central Bank of Yemen')
 
-        self.win_width = 800
-        self.win_height = 800
-        self.dis_width = self.winfo_screenwidth()
-        self.dis_height = self.winfo_screenheight()
+        self.center_window()
+        self.minsize(App.WIDTH, App.HEIGHT)
 
-        left = int(self.dis_width / 2 - self.win_width / 2)
-        top = int(self.dis_height / 2 - self.win_height / 2)
-        self.geometry(f'{self.win_width}x{self.win_height}+{left}+{top}')
+        self.main = tb.Frame(self)
+        self.main.pack(fill="both", expand=True, padx=50, pady=25)
 
-        # Upper frame
-        self.upper_frame = tb.Frame(self)
-        self.brand_label = tb.Label(self.upper_frame, text='Central Bank of Yemen', font='Ubuntu 24 bold')
-        self.register_label = tb.Label(self.upper_frame, text='Registration Form', font='Ubuntu 18')
-        # Layout
-        self.brand_label.pack()
-        self.register_label.pack(pady=10)
-        self.upper_frame.pack(pady=30)
+        self.view_register()
 
-        # Main form frame
-        self.form_frame = tb.Frame(self)
-        self.form_frame.pack()
+    def view_register(self):
+        for item in self.main.winfo_children():
+            item.destroy()
+        RegisterView(self.main, self)
 
-        # Username frame
-        self.username_frame = tb.Frame(self.form_frame)
-        self.username_label = tb.Label(self.username_frame, text='Username', font='Ubuntu 12')
-        self.username = tb.StringVar()
-        self.username_entry = tb.Entry(self.username_frame, textvariable=self.username)
-        # Layout
-        self.username_frame.columnconfigure(0, weight=1)
-        self.username_frame.rowconfigure(0, weight=1)
-        self.username_frame.rowconfigure(1, weight=1)
-        self.username_label.grid(row=0, column=0, sticky='w', pady=5)
-        self.username_entry.grid(row=1, column=0, sticky='w')
-        self.username_frame.pack(pady=15, fill='x')
+    def center_window(self):
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
 
-        # Name frame
-        self.name_frame = tb.Frame(self.form_frame)
-        self.name_label = tb.Label(self.name_frame, text='Full Name', font='Ubuntu 12')
-        self.first_name_label = tb.Label(self.name_frame, text='First Name', font='Ubuntu 8')
-        self.middle_name_label = tb.Label(self.name_frame, text='Middle Name', font='Ubuntu 8')
-        self.last_name_label = tb.Label(self.name_frame, text='Last Name', font='Ubuntu 8')
-        self.first_name = tb.StringVar()
-        self.middle_name = tb.StringVar()
-        self.last_name = tb.StringVar()
-        self.first_name_entry = tb.Entry(self.name_frame, textvariable=self.first_name)
-        self.middle_name_entry = tb.Entry(self.name_frame, textvariable=self.middle_name)
-        self.last_name_entry = tb.Entry(self.name_frame, textvariable=self.last_name)
-        # Layout
-        self.name_frame.columnconfigure(0, weight=1)
-        self.name_frame.columnconfigure(1, weight=1)
-        self.name_frame.columnconfigure(2, weight=1)
-        self.name_frame.rowconfigure(0, weight=1)
-        self.name_frame.rowconfigure(1, weight=1)
-        self.name_frame.rowconfigure(2, weight=1)
-        self.name_label.grid(row=0, column=0, sticky='w', pady=5)
-        self.first_name_entry.grid(row=1, column=0)
-        self.middle_name_entry.grid(row=1, column=1, padx=5)
-        self.last_name_entry.grid(row=1, column=2)
-        self.first_name_label.grid(row=2, column=0, sticky='w', pady=5)
-        self.middle_name_label.grid(row=2, column=1, sticky='w', pady=5, padx=5)
-        self.last_name_label.grid(row=2, column=2, sticky='w', pady=5)
-        self.name_frame.pack(pady=15, fill='x')
+        left = int(screen_width / 2 - App.WIDTH / 2)
+        top = int(screen_height / 2 - App.HEIGHT / 2)
 
-        # Date of birth and Gender frame
-        self.dob_gender_frame = tb.Frame(self.form_frame)
-        self.dob_label = tb.Label(self.dob_gender_frame, text='Date of Birth', font='Ubuntu 12')
-        self.dob = tb.StringVar()
-        self.dob_entry = tb.Entry(self.dob_gender_frame, textvariable=self.dob)
-        self.dob_info_label = tb.Label(self.dob_gender_frame, text='Use format: YYYY-MM-DD', font='Ubuntu 8')
-        self.gender_label = tb.Label(self.dob_gender_frame, text='Gender', font='Ubuntu 12')
-        self._genders = ['Male', 'Female', 'Walmart Bag']
-        self.gender = tb.StringVar()
-        self.gender_combo = tb.Combobox(self.dob_gender_frame, textvariable=self.gender)
-        self.gender_combo['values'] = self._genders
-        # Layout
-        self.dob_gender_frame.columnconfigure(0, weight=1)
-        self.dob_gender_frame.columnconfigure(1, weight=1)
-        self.dob_gender_frame.rowconfigure(0, weight=1)
-        self.dob_gender_frame.rowconfigure(1, weight=1)
-        self.dob_gender_frame.rowconfigure(2, weight=1)
-        self.dob_label.grid(row=0, column=0, sticky='w', pady=5)
-        self.dob_entry.grid(row=1, column=0, sticky='w')
-        self.dob_info_label.grid(row=2, column=0, sticky='w')
-        self.gender_label.grid(row=0, column=1, sticky='w', pady=5)
-        self.gender_combo.grid(row=1, column=1, sticky='w')
-        self.dob_gender_frame.pack(pady=15, fill='x')
-
-        # Address and Phone Number frame
-        self.adrs_phone_frame = tb.Frame(self.form_frame)
-        self.adrs_label = tb.Label(self.adrs_phone_frame, text='Residental Address', font='Ubuntu 12')
-        self.adrs = tb.StringVar()
-        self.adrs_entry = tb.Entry(self.adrs_phone_frame, textvariable=self.adrs)
-        self.phone_label = tb.Label(self.adrs_phone_frame, text='Phone Number', font='Ubuntu 12')
-        self.phone = tb.StringVar()
-        self.phone_entry = tb.Entry(self.adrs_phone_frame, textvariable=self.phone)
-        # Layout
-        self.adrs_phone_frame.columnconfigure(0, weight=1)
-        self.adrs_phone_frame.columnconfigure(1, weight=1)
-        self.adrs_phone_frame.rowconfigure(0, weight=1)
-        self.adrs_phone_frame.rowconfigure(1, weight=1)
-        self.adrs_label.grid(row=0, column=0, sticky='w', pady=5)
-        self.adrs_entry.grid(row=1, column=0, sticky='ew')
-        self.phone_label.grid(row=0, column=1, sticky='w', pady=5, padx=20)
-        self.phone_entry.grid(row=1, column=1, sticky='w', padx=20)
-        self.adrs_phone_frame.pack(pady=15, fill='x')
-
-        # Email and Password frame
-        self.email_pass_frame = tb.Frame(self.form_frame)
-        self.email_label = tb.Label(self.email_pass_frame, text='Email', font='Ubuntu 12')
-        self.email = tb.StringVar()
-        self.email_entry = tb.Entry(self.email_pass_frame, textvariable=self.email)
-        self.pass_label = tb.Label(self.email_pass_frame, text='Password', font='Ubuntu 12')
-        self.password = tb.StringVar()
-        self.pass_entry = tb.Entry(self.email_pass_frame, show='*', textvariable=self.password)
-        # Layout
-        self.email_pass_frame.columnconfigure(0, weight=1)
-        self.email_pass_frame.columnconfigure(1, weight=1)
-        self.email_pass_frame.rowconfigure(0, weight=1)
-        self.email_pass_frame.rowconfigure(1, weight=1)
-        self.email_label.grid(row=0, column=0, sticky='w', pady=5)
-        self.email_entry.grid(row=1, column=0, sticky='w')
-        self.pass_label.grid(row=0, column=1, sticky='w', pady=5)
-        self.pass_entry.grid(row=1, column=1, sticky='w')
-        self.email_pass_frame.pack(pady=15, fill='x')
-
-        # Buttons
-        self.register_btn = tb.Button(self.form_frame, text='Register User',
-                                      command=self.__user_controller.on_register_clicked)
-        self.register_btn.pack(pady=10)
+        self.geometry(f"{App.WIDTH}x{App.HEIGHT}+{left}+{top}")
---
+++ views/register_view.py
@@ -0,0 +1,153 @@
+import ttkbootstrap as tb
+import tkinter as tk
+
+
+class RegisterView(tb.Frame):
+    NEW_FIELD_PADDING = 12
+    LABEL_FONT = ("Ubuntu", 12)
+    SMALL_FONT = ("Ubuntu", 8)
+
+    def __init__(self, parent, app):
+        super().__init__(parent)
+        self.app = app
+
+        self.pack(fill="both", expand=True)
+
+        self.build_header()
+        self.build_form()
+        self.build_buttons()
+
+    def build_header(self):
+        header = tb.Frame(self)
+        header.pack()
+
+        tb.Label(header, text="Central Bank of Yemen", font=("Ubuntu", 24, "bold")).pack()
+        tb.Label(header, text="Registration Form", font=("Ubuntu", 18)).pack(pady=RegisterView.NEW_FIELD_PADDING)
+
+    def build_form(self):
+        self.form_frame = tb.Frame(self)
+        self.form_frame.pack()
+
+        self.build_username()
+        self.build_full_name()
+        self.build_dob_gender()
+        self.build_address_phone()
+        self.build_email_password()
+
+    def build_username(self):
+        frame = tb.Frame(self.form_frame)
+
+        frame.pack(fill="x", pady=RegisterView.NEW_FIELD_PADDING)
+        frame.columnconfigure(0, weight=1)
+        frame.rowconfigure((0, 1), weight=1)
+
+        self.username = tb.StringVar()
+
+        tb.Label(frame, text="Username", font=RegisterView.LABEL_FONT).grid(row=0, column=0, sticky="w")
+        tb.Entry(frame, textvariable=self.username).grid(row=1, column=0, sticky="ew")
+
+    def build_full_name(self):
+        frame = tb.Frame(self.form_frame)
+
+        frame.pack(fill="x", pady=RegisterView.NEW_FIELD_PADDING)
+        frame.columnconfigure((0, 1, 2), weight=1)
+        frame.rowconfigure((0, 1, 2), weight=1)
+
+        self.first_name = tb.StringVar()
+        self.middle_name = tb.StringVar()
+        self.last_name = tb.StringVar()
+
+        (tb.Label(frame, text="Full Name", font=RegisterView.LABEL_FONT)
+         .grid(row=0, column=0, sticky="w", columnspan=3))
+
+        tb.Entry(frame, textvariable=self.first_name).grid(row=1, column=0, sticky="ew")
+        tb.Label(frame, text="First Name", font=RegisterView.SMALL_FONT).grid(row=2, column=0, sticky="w")
+
+        (tb.Entry(frame, textvariable=self.middle_name)
+         .grid(row=1, column=1, sticky="ew", padx=RegisterView.NEW_FIELD_PADDING))
+        (tb.Label(frame, text="Middle Name", font=RegisterView.SMALL_FONT)
+         .grid(row=2, column=1, sticky="w", padx=RegisterView.NEW_FIELD_PADDING))
+
+        tb.Entry(frame, textvariable=self.last_name).grid(row=1, column=2, sticky="ew")
+        tb.Label(frame, text="Last Name", font=RegisterView.SMALL_FONT).grid(row=2, column=2, sticky="w")
+
+    def build_dob_gender(self):
+        frame = tb.Frame(self.form_frame)
+
+        frame.pack(fill="x", pady=self.NEW_FIELD_PADDING)
+        frame.columnconfigure((0, 1), weight=1)
+        frame.rowconfigure((0, 1, 2), weight=1)
+
+        self.dob = tb.StringVar()
+        self.gender = tb.StringVar()
+
+        tb.Label(frame, text="Date of Birth", font=self.LABEL_FONT).grid(row=0, column=0, sticky="w")
+        tb.Entry(frame, textvariable=self.dob).grid(row=1, column=0, sticky="ew")
+        tb.Label(frame, text="Use format: YYYY-MM-DD", font=self.SMALL_FONT).grid(row=2, column=0, sticky="w")
+
+        (tb.Label(frame, text="Gender", font=self.LABEL_FONT)
+         .grid(row=0, column=1, sticky="w", padx=(RegisterView.NEW_FIELD_PADDING, 0)))
+        tb.Combobox(
+            frame,
+            textvariable=self.gender,
+            values=["Male", "Female", "Walmart Bag"],
+            state="readonly"
+        ).grid(row=1, column=1, sticky="ew", padx=(RegisterView.NEW_FIELD_PADDING, 0))
+
+    def build_address_phone(self):
+        frame = tb.Frame(self.form_frame)
+
+        frame.pack(fill="x", pady=self.NEW_FIELD_PADDING)
+        frame.columnconfigure(0, weight=4)
+        frame.columnconfigure(1, weight=1)
+        frame.rowconfigure((0, 1), weight=1)
+
+        self.address = tb.StringVar()
+        self.phone = tb.StringVar()
+
+        tb.Label(frame, text="Residential Address", font=self.LABEL_FONT).grid(row=0, column=0, sticky="w")
+        tb.Entry(frame, textvariable=self.address).grid(row=1, column=0, sticky="ew")
+
+        (tb.Label(frame, text="Phone Number", font=self.LABEL_FONT)
+         .grid(row=0, column=1, sticky="w", padx=(RegisterView.NEW_FIELD_PADDING, 0)))
+        (tb.Entry(frame, textvariable=self.phone)
+         .grid(row=1, column=1, sticky="ew", padx=(RegisterView.NEW_FIELD_PADDING, 0)))
+
+    def build_email_password(self):
+        frame = tb.Frame(self.form_frame)
+
+        frame.pack(fill="x", pady=self.NEW_FIELD_PADDING)
+        frame.columnconfigure((0, 1), weight=1)
+        frame.rowconfigure((0, 1), weight=1)
+
+        self.email = tb.StringVar()
+        self.password = tb.StringVar()
+
+        tb.Label(frame, text="Email", font=self.LABEL_FONT).grid(row=0, column=0, sticky="w")
+        tb.Entry(frame, textvariable=self.email).grid(row=1, column=0, sticky="ew")
+
+        (tb.Label(frame, text="Password", font=self.LABEL_FONT)
+         .grid(row=0, column=1, sticky="w", padx=(RegisterView.NEW_FIELD_PADDING, 0)))
+        (tb.Entry(frame, textvariable=self.password, show="*")
+         .grid(row=1, column=1, sticky="ew", padx=(RegisterView.NEW_FIELD_PADDING, 0)))
+
+    def build_buttons(self):
+        tb.Button(
+            self.form_frame,
+            text="Register User",
+            command=self.app.user_controller.on_register_clicked
+        ).pack(pady=RegisterView.NEW_FIELD_PADDING)
+
+    def get_form_data(self):
+        return {
+            "username": self.username.get(),
+            "first_name": self.first_name.get(),
+            "middle_name": self.middle_name.get(),
+            "last_name": self.last_name.get(),
+            "dob": self.dob.get(),
+            "gender": self.gender.get(),
+            "address": self.address.get(),
+            "phone": self.phone.get(),
+            "email": self.email.get(),
+            "password": self.password.get(),
+        }
